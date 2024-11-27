257dbd1e2e03a100646af90a439e741dd83c204c
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-11-27 13:38:27 +0000

    Update: reduce MaxLength:

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,5 +1,4 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
-    # This is the base class for all channels. It provides a number of methods for managing the connection to the client, subscribing to streams, and broadcasting messages to the client.
   end
 end
