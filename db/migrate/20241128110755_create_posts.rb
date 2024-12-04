ca908b895e1d14bbe99b3f4596545c7b5f94771a
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-12-04 03:47:18 +0000

    Add Data models to the database

--- db/migrate/20241128110755_create_posts.rb
+++ db/migrate/20241128110755_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-      t.references :author, null: false, foreign_key: { to_table: :users }, null: false
+      t.references :author, foreign_key: { to_table: :users }, null: false
       t.text :text
       t.string :title, null: false
       t.integer :comments_counter, null: false, default: 0
--- db/migrate/20241128112020_create_likes.rb
+++ db/migrate/20241128112020_create_likes.rb
@@ -1,9 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.1]
   def change
     create_table :likes do |t|
-      t.references :post, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
-      t.text :text
+      t.references :post, foreign_key: true, null: false
+      t.references :user, foreign_key: true, null: false
 
       t.timestamps
     end
