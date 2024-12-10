6b9a3a21580a9f7fc7e2c3c174d4d8590be3e45c
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-12-10 16:59:20 +0000

    modified all db and config/initialzers files - complete

--- db/migrate/20241128112020_create_likes.rb
+++ db/migrate/20241128112020_create_likes.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# ApplicationHelper provides helper methods for views.
 class CreateLikes < ActiveRecord::Migration[7.1]
   def change
     create_table :likes do |t|
