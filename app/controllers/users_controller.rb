cce1e96c6a2f3c01358dc17b09a1114005494bfd
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-12-16 15:05:23 +0000

    Remove frozen string literal comments from users and application helper files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # UsersController is a controller for users.
 class UsersController < ApplicationController
   def show
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # UsersHelper provides helper methods for views.
 module UsersHelper
 end
