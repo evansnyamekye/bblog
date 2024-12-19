d5dc166c824c674d3a92908f7a50b2ae70f9b597
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-12-19 07:58:40 +0000

    Enhance user profile view with improved layout and recent posts display; update routes for nested post resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  get '/users/:id', to: 'users#show'
+  resources :users do
+    resources :posts
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
