b812d435fc32dd07333e5bd5c14833398e5d22db
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-12-20 16:19:35 +0000

    Refactor User model: streamline associations and add method for recent posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,16 @@
 # ApplicationHelper provides helper methods for views.
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id', dependent: :destroy
-  has_many :comments, dependent: :destroy
+  has_many :posts, foreign_key: :author_id
+  has_many :likes
+  has_many :comments
 
   validates :name, presence: true
+
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def recent_posts
-    post.order(created_at(:desc)).limit(5)
+  def three_recent_posts
+    posts.order(created_at: :desc).limit(3)
   end
 
-  def recent_comments
-    comments.order(created_at(:desc)).liimit(3)
-  end
-
-  def recent_likes
-    recent.like(created_at(:desc)).limit(3)
-  end
-
-  def likes?(post)
-    likes.exists?(post_id: post.id)
-  end
-
-  def admin?
-    role == 'admin'
-  end
+  
 end
