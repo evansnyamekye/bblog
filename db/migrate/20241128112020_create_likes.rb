8f3132453e4a3737ff972bff50fbaba9d588fcde
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-12-20 16:39:27 +0000

    Refactor Likes and Comments migrations: remove null constraints and foreign keys for flexibility

--- db/migrate/20241128112020_create_likes.rb
+++ db/migrate/20241128112020_create_likes.rb
@@ -1,12 +1,9 @@
-# frozen_string_literal: true
-
-# CreateLikes is a migration that creates a table called likes with a user_id and post_id.
 class CreateLikes < ActiveRecord::Migration[7.1]
   def change
     create_table :likes do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
-
+      t.references :user
+      t.references :post
+   
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_28_112020) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_20_103235) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comments", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
+    t.bigint "user_id"
+    t.bigint "post_id"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -25,8 +32,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_28_112020) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.bigint "post_id", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
+    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
@@ -34,11 +41,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_28_112020) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.bigint "author_id", null: false
+    t.string "title"
     t.text "text"
-    t.string "title", null: false
-    t.integer "comments_counter", default: 0, null: false
-    t.integer "likes_counter", default: 0, null: false
+    t.integer "likes_counter", default: 0
+    t.integer "comments_counter", default: 0
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
@@ -48,14 +55,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_28_112020) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter", default: 0, null: false
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "attribute_name"
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users", column: "author_id"
 end
